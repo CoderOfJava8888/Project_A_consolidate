ae83d377c6114473f1ba4ccd2dcf79c0a5deab81
CoderOfJava8888 <88905316+CoderOfJava8888@users.noreply.github.com>
2023-01-18 16:44:53 -0600

    Update LiveBarPriorClassification.java

--- src/strategy/LiveBarPriorClassification.java
+++ src/strategy/LiveBarPriorClassification.java
@@ -1,28 +1,71 @@
 package strategy;
 
-
 public class LiveBarPriorClassification {
+
+    private double open, high, low, close;
+    private int minute;
     private long volume;
     private int count;
     private double wap;
     private double tesla3, tesla6, tesla9;
     private String decision;
 
-    
-    
-    public LiveBarPriorClassification(long volume, int count, double wap, double tesla3, double tesla6, double tesla9, String decision) {
-    this.volume=volume;
-    this.count=count;
-    this.wap=wap;
-    this.tesla3=tesla3;
-    this.tesla6=tesla6;
-    this.tesla9=tesla9;
-    this.decision=decision;
-    
+    public LiveBarPriorClassification(double open, double high, double low, double close, int minute, long volume, int count, double wap, double tesla3, double tesla6, double tesla9, String decision) {
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.minute = minute;
+        this.volume = volume;
+        this.count = count;
+        this.wap = wap;
+        this.tesla3 = tesla3;
+        this.tesla6 = tesla6;
+        this.tesla9 = tesla9;
+        this.decision = decision;
+
     }
 
+    public double getOpen() {
+        return open;
+    }
 
-    
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
     public long getVolume() {
         return volume;
     }
@@ -78,12 +121,5 @@ public class LiveBarPriorClassification {
     public void setDecision(String decision) {
         this.decision = decision;
     }
-    
-    
-
-
-
-
- 
 
 }
